6af51f48f4a7f93cc71f3ec2497d2251f243d936
codersimone <97359905+codersimone@users.noreply.github.com>
2022-07-21 12:28:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 document.querySelector ("button").addEventListener ("click", function () {
+    //создаем пустой массив и генерируем 10 случайных чисел в нужном диапозоне при попощи цикла for
     let generatedArray = [];
     for (let i = 0; i < 10; i++) generatedArray[i] = Math.floor(Math.random() * 21 - 10);
     document.querySelector('#outResult').innerHTML = 'Сгенерировали: ' + generatedArray;
+    //находим минимальное значение в массиве
     let min = Math.min.apply(null, generatedArray);
     document.querySelector('#outMin').innerHTML = 'Минимальное: ' + min;
+    //находим максимальное значение в массиве
     let max = Math.max.apply(null, generatedArray);
     document.querySelector('#outMax').innerHTML = 'Максимальное: ' + max;
-// найти среднее арифметическое значение массива элементов (сумму элементов поделить на количество элементов)
+    // находим среднее арифметическое значение массива (сумму элементов поделить на количество элементов)
     let sum = 0; // переменная с нулевым значением, далее цикл for, метод length (устанавливаю длину массива), деление и вывод результата
     for (let i = 0; i < generatedArray.length; i++) {
         sum += generatedArray[i];
     }
     let result = sum / generatedArray.length;
     document.querySelector('#outArithmeticMean').innerHTML = 'Среднее арифметическое: ' + result;
+    //находим сумму элементов массива
     let findSum = generatedArray.reduce(function(sum, num) {
         return sum + num;
-    }); //в learn.js в разделе метода массива reduce в синтаксисе поиска суммы значений массива пишется "}, 0);" (присваивается нулевое значение?!) и при подсчете суммы значений работает и с 0 и без него, но при поиске произведения значений см. ниже
+    }, 0);
     document.querySelector('#outSum').innerHTML = 'Сумма чисел: ' + findSum;
+    //находим произведение элементов массива
     let findProduct = generatedArray.reduce(function(product, num) {
+        console.log(product);
         return product * num;
-    }); //если ставлю "}, 0);", то результат всегда 0 (значения умножаются на 0 и получается 0?!) - при умножении не ставить 0, а при сложении ставить?
+    }, 1);
     document.querySelector('#outProduct').innerHTML = 'Произведение чисел: ' + findProduct;
 });
